3f541aa0e33a829b5e3f0f2e5e09c27a4e04bded
Egor <yegeraskin13@gmail.com>
2021-02-27 12:53:19 +0400

    1. Spring mock [#435417]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,18 +1,25 @@
 package ru.job4j.forum.control;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.junit.jupiter.api.Test;
-
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -20,12 +27,27 @@ public class PostControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService posts;
+
     @Test
     @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void shouldReturnDefaultMessageGetMethod() throws Exception {
         this.mockMvc.perform(get("/edit"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessagePostMethod() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name","Куплю ладу-грант. Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
     }
 }
--- src/test/java/ru/job4j/forum/control/SubPostControlTest.java
+++ src/test/java/ru/job4j/forum/control/SubPostControlTest.java
@@ -1,24 +1,38 @@
 package ru.job4j.forum.control;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.model.SubPost;
+import ru.job4j.forum.service.PostService;
+import ru.job4j.forum.service.SubPostService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class SubPostControlTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private SubPostService subServ;
 
     @Test
     @WithMockUser
@@ -28,5 +42,18 @@ public class SubPostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessagePostMethod() throws Exception {
+        this.mockMvc.perform(post("/subpost/save")
+                .param("header","Куплю ладу-грант. Дорого.")
+                .param("postId","1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<SubPost> argument = ArgumentCaptor.forClass(SubPost.class);
+        verify(subServ).save(argument.capture());
+        assertThat(argument.getValue().getHeader(), is("Куплю ладу-грант. Дорого."));
     }
 }
